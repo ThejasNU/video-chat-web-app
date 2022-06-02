9202ea1de4ea157985b302d37c3fe86a25f6ab2e
ThejasNU <thejasnu@gmail.com>
2022-06-02 15:28:39 +0530

    updated link on client side

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:7777");
+const socket = io("https://webrtc-videochat-web-app.herokuapp.com/");
 
 const ContextProvider = ({ children }) => {
 	const [callAccepted, setCallAccepted] = useState(false);
