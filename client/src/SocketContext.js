378178060a5f67c7a925505b217796ee2ab6d6cc
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-01-02 22:35:26 +0530

    Changed backend URL

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("https://webrtc-videochat-web-app.herokuapp.com/");
+const socket = io("https://webrtc-videochat-webapp-backend.vercel.app/");
 
 const ContextProvider = ({ children }) => {
 	const [callAccepted, setCallAccepted] = useState(false);
